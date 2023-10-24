f9c73d81a66bb2133bc18b97583da3aaba603a76
Carlos Afonso <carlos.flach15@gmail.com>
2023-10-24 22:11:26 +0200

    feat: Adds the button in NewTask Component

--- src/Components/NewTask.tsx
+++ src/Components/NewTask.tsx
@@ -1,9 +1,13 @@
 import styles from './NewTask.module.css';
+import Plus from '../assets/plus.svg?react';
 
 export const NewTask = () => {
 	return (
 		<div className={styles.container}>
 			<input placeholder='Adicione uma nova tarefa' type='text' />
+			<button>
+				Criar <Plus />
+			</button>
 		</div>
 	);
 };
